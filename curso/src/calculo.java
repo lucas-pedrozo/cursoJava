b62ad9755ceb25d003e076c59444338e3eaaef50
lucas-pedrozo <lucaspedroozoo@hotmail.com>
2024-08-16 21:49:21 -0300

    versão 1.2

---
+++ curso/src/calculo.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class calculo {
+
+    public static void main(String[] args) {
+
+        Scanner prompt = new Scanner(System.in);
+
+        System.out.print("Informe os segundos: ");
+        int segundos = prompt.nextInt();
+
+        int horas = segundos/60/60;
+
+         segundos = segundos - (horas*60*60);
+
+        int minutos = segundos/60;
+
+         segundos =  segundos - (minutos*60);
+
+        System.out.println("horas:" + horas + " minutos:" + minutos + " segundo:" + segundos);
+    }
+
+}
--- curso/src/index.java
+++ curso/src/index.java
@@ -1,7 +1,22 @@
+import java.util.Scanner;
+
 public class index {
 
-    public static void main(String[] args) {
-        System.out.println("Hello Word");
-    }
+  public static void main(String[] args) {
 
+    Scanner prompt = new Scanner(System.in);
+
+    System.out.print("Informe as Horas: ");
+    int horas = prompt.nextInt();
+
+    System.out.print("Informe as minutos: ");
+    int minutos = prompt.nextInt();
+
+    int segundos = prompt.nextInt();
+    System.out.print("Informe as segundos: ");
+
+    int resultado = (horas * 60 * 60) + (minutos * 60) + segundos;
+    System.out.println("resultado: " + resultado);
+
+  }
 }
